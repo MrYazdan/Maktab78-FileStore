9fc20ed3e307bbad84a560debaa329fcb9ff5876
mryazdan <mrrezayazdani@yahoo.com>
2022-08-26 21:32:37 +0430

    Added some method to router and route to easy use them

--- core/router.py
+++ core/router.py
@@ -6,6 +6,7 @@ class CallBack:
     """
         ...
     """
+
     def __init__(self, package, function, *args, **kwargs):
         self.function = getattr(import_module(package), function)
         self.args = args
@@ -19,22 +20,43 @@ class Route:
     """
         ...
     """
+
     def __init__(self, name, description=None, callback: CallBack = None, children=()) -> None:
         self.name = name
         self.description = description
         self.callback = callback
-        ...
+        # ...
         if not callback: self.children = children
+
+    def run(self):
+        # ...
+        if children := self.children:
+            for child in children:
+                child: Route
+                print(f"{children.index(child) + 1}. {child.name}")
+
+            index = int(input("\n>> ")) - 1
+            # ...
+
+            children[index]: Route
+            children[index].run()
+
+        else:
+            # ...
+            self.callback: CallBack
+            self.callback.call()
 
 
 class Router:
     """
         ...
     """
-    def __init__(self, name, route):
+
+    def __init__(self, name: str, route: Route) -> None:
         self.name = name
         self.route = route
 
     def generate(self) -> None:
         clear()
-        ...
+        self.route.run()
+        # ...
